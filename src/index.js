21a0bda10b8634661da9521eff75f1994f7bfc23
Harsh18064 <singh.harsh10012001@gmail.com>
2022-03-14 01:33:41 +0530

    First commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App';
+import store from './app/store';
+
+import 'antd/dist/antd.css';
+
+// hook our app to root div
+// we are wrapping our app with router and provider(where we set store variable = {store})
+// by doing this every component inside our App will have access to
+// the store variable
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
